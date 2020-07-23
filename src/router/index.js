a454043685bf1e5712c69827cb2523200d535e97
zhaoxiaofei <123@qq.com>
2020-07-23 18:49:19 +0800

    发布文章-组件与路由已完成

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Welcome from '@/views/welcome'
 import NotFound from '@/views/404'
 import Article from '@/views/article'
 import Image from '@/views/image'
+import Publish from '@/views/publish'
 
 Vue.use(VueRouter);
 
@@ -32,7 +33,9 @@ const routes = [
             // 内容管理
             { path: '/article', component: Article },
             // 素材管理
-            { path: '/image', component: Image }
+            { path: '/image', component: Image },
+            // 发布文章
+            { path: '/publish', component: Publish }
         ]
     },
     // 通配规则，404组件
---
+++ src/views/publish/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-publish">发布文章</div>
+</template>
+
+<script>
+export default {
+  name: "page-publish"
+};
+</script>
+
+<style>
+</style>
